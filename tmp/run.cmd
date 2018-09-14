c17b4cec71fc7f4f3c65d5df6ec63cad5138787f
Özkan Bilge <39211596+ozkanbilge@users.noreply.github.com>
2018-09-14 11:17:52 +0300

    Add files via upload

---
+++ tmp/run.cmd
@@ -0,0 +1,2 @@
+del "D:\DELPHI\Xpath Blind Explorer\release\tmp\result.htm"
+"D:\DELPHI\Xpath Blind Explorer\release\curl.exe" --config "D:\DELPHI\Xpath Blind Explorer\release\tmp\curl.tmp_config" -k >"D:\DELPHI\Xpath Blind Explorer\release\tmp\result.htm"
